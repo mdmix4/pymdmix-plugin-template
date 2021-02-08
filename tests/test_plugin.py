e315189c5afdf5f46f16b53bcd2eb2cbf38439f0
ggutierrez-bio <ggutierrez.bio@gmail.com>
2021-02-09 00:10:31 +0100

    linting

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,5 +1,6 @@
 from pymdmix_core.plugin import PluginManager
 
+
 def test_plugin_manager_load_plugin():
     plugin_manager = PluginManager()
     plugin_manager.load_plugin("pymdmix_plugin_template")
