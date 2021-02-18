23271fd41663aaa80a4d42135077a9784fdad018
ggutierrez-bio <ggutierrez.bio@gmail.com>
2021-02-19 00:29:33 +0100

    update templates to fit new plugin interfaces from core

--- pymdmix_plugin_template/crud_plugin_template.py
+++ pymdmix_plugin_template/crud_plugin_template.py
@@ -1,14 +1,22 @@
 from argparse import ArgumentParser, Namespace
-from pymdmix_core.plugin.base import Plugin
+from sqlalchemy import Column, Integer
+from pymdmix_core.orm import BaseModel
+from pymdmix_core.plugin.crud import CRUDPlugin
 
 
-class PluginTemplate(Plugin):
+class PluginTemplateModel(BaseModel):
+    __tablename__ = "plugin_template_models"
+    id = Column(Integer(), primary_key=True)
 
-    NAME = "plugin_template"
-    HELP_STRING: str = "plugin_template"
+
+class CrudPluginTemplate(CRUDPlugin):
+
+    NAME = "crud_plugin_template"
+    HELP_STRING: str = "crud_plugin_template"
     LOAD_CONFIG: bool = False
-    CONFIG_FILE: str = "pymdmix_plugin_template.yml"
+    CONFIG_FILE: str = "pymdmix_crud_plugin_template.yml"
     ALLOW_EMPTY_ACTION = False
+    CLASS = PluginTemplateModel
 
     def __init__(self, master_parser: ArgumentParser) -> None:
         super().__init__(master_parser)
