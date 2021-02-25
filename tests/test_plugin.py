2d49fe62edd1feaf39efd545c8c3c05da7ff55dc
alvaroSmorras <alvaro.serrano.morras@gmail.com>
2021-02-25 17:35:22 +0100

    initial commit

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,8 +1,11 @@
 from unittest import mock
+from test.conftest import db_patch
 from pymdmix_core.plugin import PluginManager
 from pymdmix_core.parser import get_mdmix_parser, MDMIX_PARSER
 from pymdmix_plugin_template.crud_plugin_template import CrudPluginTemplate
 
+
+@db_patch
 
 def test_plugin_manager_load_plugin_template():
     plugin_manager = PluginManager()
